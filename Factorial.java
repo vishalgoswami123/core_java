75938b5e61dce5dd5180e7e595997c01f80e3e23
vishalgoswami123 <137057005+vishalgoswami123@users.noreply.github.com>
2025-07-02 21:09:13 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+// Factorial.java
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + num + " is: " + factorial);
+        sc.close();
+    }
+}
---
+++ MaxNumber.java
@@ -0,0 +1,8 @@
+// MaxNumber.java
+public class MaxNumber {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum number is: " + max);
+    }
+}
